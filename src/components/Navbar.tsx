7cca37bd2ac01164a12f6e9eb5aac5e97a3e3fc1
Todd <luiscatodd@gmail.com>
2022-02-09 14:55:29 -0600

    Bootstrap Types

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Navbar from 'react-bootstrap/Navbar'
+
+type Props = {
+    current: string;
+}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav>Navbar</nav>
+  )
+}
+
+export default Navbar
